ead6700ea74643bb2165632a0ef1a19702362401
丁丁 <Kutinging@users.noreply.github.com>
2018-10-15 14:15:12 +0800

    test.1

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
 class config:
     Domain = '127.0.0.1:5000'
-    Pixiv = {'username':os.environ['Pixiv_username'],'password':os.environ['Pixiv_password'],'refresh_token':os.environ['Pixiv_refresh_token']}
+    Pixiv = {'username':os.environ['Pixiv_username'],'password':os.environ['Pixiv_password'],'refresh_token':''}
+    if os.environ['Pixiv_refresh_token']:
+        Pixiv['refresh_token'] = os.environ['Pixiv_refresh_token']
+    
